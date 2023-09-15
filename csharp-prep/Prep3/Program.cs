4a8c6ecbf6c1291f4274b868b5d9cbcef88ae8aa
BraxtonGoode <tjbgoode@gmail.com>
2023-09-15 13:28:04 -0700

    Finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        //random number generator//
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1,20);
+
+       
+        
+
+        int numGuess = 0;
+
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            numGuess = int.Parse(guess);
+
+            if (numGuess > randomNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (numGuess < randomNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (numGuess != randomNumber);
+
+        
+
+        
+
+        
+
     }
 }
