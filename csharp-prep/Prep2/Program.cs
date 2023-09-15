61f4c8d510d0729ef233f31b29794f6f023a30a4
BraxtonGoode <tjbgoode@gmail.com>
2023-09-15 13:04:25 -0700

    finished Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,47 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        //determining grades//
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+
+        string letterGrade = "";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"You have an {letterGrade}");
+
+        //determing if you passed//
+        if (grade >= 70)
+        {
+            Console.WriteLine("You have passed the class!");
+        }
+        else 
+        {
+            Console.WriteLine("You did not pass the class. Please try again next semester!");
+        }
+
+
+
     }
 }
